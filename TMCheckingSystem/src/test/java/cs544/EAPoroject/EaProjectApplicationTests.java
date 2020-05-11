87cae4a240c54700bf021f75f354deed7eb6f1be
Moustafa Bahnasawy <moustafa.a.galal@gmail.com>
2020-05-11 13:57:56 -0500

    Creating Inialize version of the project

---
+++ TMCheckingSystem/src/test/java/cs544/EAPoroject/EaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cs544.EAPoroject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
